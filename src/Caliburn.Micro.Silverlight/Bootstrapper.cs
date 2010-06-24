ad77119787a5f781595a34d5022ab1f1dd08b416
EisenbergEffect <none@none>
2010-06-24 13:25:49 -0400

    Added some more code documentation.

--- src/Caliburn.Micro.Silverlight/Bootstrapper.cs
+++ src/Caliburn.Micro.Silverlight/Bootstrapper.cs
@@ -7,14 +7,25 @@
     using System.Windows;
     using System.Windows.Controls;
 
+    /// <summary>
+    /// Instantiate this class in order to configure the framework.
+    /// </summary>
     public class Bootstrapper
     {
 #if SILVERLIGHT
+        /// <summary>
+        /// Indicates whether or not the framework is in design-time mode.
+        /// </summary>
         public static readonly bool IsInDesignMode = DesignerProperties.GetIsInDesignMode(new UserControl());
 #else
+        /// <summary>
+        /// Indicates whether or not the framework is in design-time mode.
+        /// </summary>
         public static readonly bool IsInDesignMode = DesignerProperties.GetIsInDesignMode(new DependencyObject());
 #endif
-
+        /// <summary>
+        /// Creates an instance of the bootstrapper.
+        /// </summary>
         public Bootstrapper()
         {
             if(!IsInDesignMode)
@@ -32,8 +43,15 @@
             IoC.BuildUp = BuildUp;
         }
 
+        /// <summary>
+        /// Override to configure the framework and setup your IoC container.
+        /// </summary>
         protected virtual void Configure() { }
 
+        /// <summary>
+        /// Override to tell the framework where to find assemblies to inspect for views, etc.
+        /// </summary>
+        /// <returns>A list of assemblies to inspect.</returns>
         protected virtual IEnumerable<Assembly> SelectAssemblies()
         {
 #if SILVERLIGHT
@@ -43,19 +61,38 @@
 #endif
         }
 
+        /// <summary>
+        /// Override this to provide an IoC specific implementation.
+        /// </summary>
+        /// <param name="service">The service to locate.</param>
+        /// <param name="key">The key to locate.</param>
+        /// <returns>The located service.</returns>
         protected virtual object GetInstance(Type service, string key)
         {
             return Activator.CreateInstance(service);
         }
 
+        /// <summary>
+        /// Override this to provide an IoC specific impelentation
+        /// </summary>
+        /// <param name="service">The service to locate.</param>
+        /// <returns>The located services.</returns>
         protected virtual IEnumerable<object> GetAllInstances(Type service)
         {
             return new[] { Activator.CreateInstance(service) };
         }
 
+        /// <summary>
+        /// Override this to provide an IoC specific implementation.
+        /// </summary>
+        /// <param name="instance">The instance to perform injection on.</param>
         protected virtual void BuildUp(object instance) {}
     }
 
+    /// <summary>
+    /// A strongly-typed version of <see cref="Bootstrapper"/> that specifies the type of rootmodel to create for the application.
+    /// </summary>
+    /// <typeparam name="TRootModel">The type of root model for the application.</typeparam>
     public class Bootstrapper<TRootModel> : Bootstrapper
     {
         internal override void Start()
@@ -64,6 +101,11 @@
             Application.Current.Startup += OnStartup;
         }
 
+        /// <summary>
+        /// Override this to add custom behavior to execute after the application starts.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The args.</param>
         protected virtual void OnStartup(object sender, StartupEventArgs e)
         {
             var viewModel = IoC.Get<TRootModel>();
--- src/Caliburn.Micro.Silverlight/CompositeCloseStrategy.cs
+++ src/Caliburn.Micro.Silverlight/CompositeCloseStrategy.cs
@@ -3,18 +3,31 @@
     using System;
     using System.Collections.Generic;
 
+    /// <summary>
+    /// Used to gather the results from multiple child elements which may or may not prevent closing.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class CompositeCloseStrategy<T>
     {
         readonly Action<bool> callback;
         readonly IEnumerator<T> enumerator;
         bool finalResult = true;
 
+        /// <summary>
+        /// Creates a new instance of <see cref="CompositeCloseStrategy{T}"/>
+        /// </summary>
+        /// <param name="enumerator">Enumerates the items that are requesting close.</param>
+        /// <param name="callback">The action to call when all enumeration is complete and the close results are aggregated.</param>
         public CompositeCloseStrategy(IEnumerator<T> enumerator, Action<bool> callback)
         {
             this.callback = callback;
             this.enumerator = enumerator;
         }
 
+        /// <summary>
+        /// Executes the strategy.
+        /// </summary>
+        /// <param name="result">The result of the previous guard check.</param>
         public void Execute(bool result = true)
         {
             finalResult = finalResult && result;
