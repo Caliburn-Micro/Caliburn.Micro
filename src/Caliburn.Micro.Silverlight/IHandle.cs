e553799837d339c2a0a199384b5ce1c46a7dfcf1
EisenbergEffect <none@none>
2010-07-23 16:07:50 -0400

    Closed ticket #8 by removing contravariance from IHandle in the WP7 version.

--- src/Caliburn.Micro.Silverlight/IHandle.cs
+++ src/Caliburn.Micro.Silverlight/IHandle.cs
@@ -4,7 +4,11 @@
     /// Denotes a class which can handle a particular type of message.
     /// </summary>
     /// <typeparam name="TMessage">The type of message to handle.</typeparam>
+#if WP7
+    public interface IHandle<TMessage>
+#else
     public interface IHandle<in TMessage>
+#endif
     {
         /// <summary>
         /// Handles the message.
