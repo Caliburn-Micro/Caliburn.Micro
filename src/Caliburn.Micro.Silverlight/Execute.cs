8e505e709e829199f8bdc5bf5b77e7e910e9dbdf
EisenbergEffect <none@none>
2010-12-22 10:57:44 -0500

    Fixed a bug in the Execute.OnUIThread method.

--- src/Caliburn.Micro.Silverlight/Execute.cs
+++ src/Caliburn.Micro.Silverlight/Execute.cs
@@ -1,6 +1,8 @@
 ﻿namespace Caliburn.Micro
 {
     using System;
+    using System.Reflection;
+    using System.Threading;
     using System.Windows;
     using System.Windows.Threading;
 
@@ -18,14 +20,37 @@
         {
 #if SILVERLIGHT
             var dispatcher = Deployment.Current.Dispatcher;
-#else
-            var dispatcher = Dispatcher.CurrentDispatcher;
-#endif
-            executor = action =>{
+
+            executor = action => {
                 if(dispatcher.CheckAccess())
                     action();
-                else dispatcher.BeginInvoke(action);
+                else {
+                    var waitHandle = new ManualResetEvent(false);
+                    Exception exception = null;
+                    dispatcher.BeginInvoke(() => {
+                        try {
+                            action();
+                        }
+                        catch(Exception ex) {
+                            exception = ex;
+                        }
+                        waitHandle.Set();
+                    });
+                    waitHandle.WaitOne();
+                    if(exception != null)
+                        throw new TargetInvocationException("An error occurred while dispatching a call to the UI Thread", exception);
+                }
             };
+#else
+            var dispatcher = Dispatcher.CurrentDispatcher;
+
+            executor = action => {
+                if(dispatcher.CheckAccess())
+                    action();
+                else dispatcher.Invoke(action);
+            };
+#endif
+
         }
 
         /// <summary>
