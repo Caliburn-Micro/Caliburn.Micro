3c91daa559ce72c37d9749d449268714fb8f3728
EisenbergEffect <none@none>
2011-01-01 06:51:31 -0500

    Oops. Forgot to add a file on the last commit.

---
+++ src/Caliburn.Micro.Silverlight/Mouse.cs
@@ -0,0 +1,21 @@
+﻿namespace Caliburn.Micro {
+    using System.Windows;
+
+    /// <summary>
+    /// A mouse helper utility.
+    /// </summary>
+    public static class Mouse {
+        /// <summary>
+        /// The current position of the mouse.
+        /// </summary>
+        public static Point Position { get; set; }
+
+        /// <summary>
+        /// Initializes the mouse helper with the UIElement to use in mouse tracking.
+        /// </summary>
+        /// <param name="element">The UIElement to use for mouse tracking.</param>
+        public static void Initialize(UIElement element) {
+            element.MouseMove += (s, e) => { Position = e.GetPosition(null); };
+        }
+    }
+}
