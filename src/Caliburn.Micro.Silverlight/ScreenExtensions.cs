23a9380b05eb608ac0a1788934b40a2926272a40
EisenbergEffect <none@none>
2011-02-18 10:17:14 -0500

    Added ActivateWith, DeactivateWith and ConductWith extension methods to attach arbitrary items into the lifecycle of another item.

--- src/Caliburn.Micro.Silverlight/ScreenExtensions.cs
+++ src/Caliburn.Micro.Silverlight/ScreenExtensions.cs
@@ -1,5 +1,7 @@
 ﻿namespace Caliburn.Micro
 {
+    using System;
+
     /// <summary>
     /// Hosts extension methods for <see cref="IScreen"/> classes.
     /// </summary>
@@ -46,6 +48,56 @@
         public static void CloseItem<T>(this ConductorBase<T> conductor, T item)
         {
             conductor.DeactivateItem(item, true);
+        }
+
+        ///<summary>
+        /// Activates a child whenever the specified parent is activated.
+        ///</summary>
+        ///<param name="child">The child to activate.</param>
+        ///<param name="parent">The parent whose activation triggers the child's activation.</param>
+        public static void ActivateWith(this IActivate child, IActivate parent) {
+            EventHandler<ActivationEventArgs> handler = (s, e) => child.Activate();
+            parent.Activated += handler;
+
+            var deactivator = parent as IDeactivate;
+            if(deactivator != null) {
+                EventHandler<DeactivationEventArgs> handler2 = null;
+                handler2 = (s, e) => {
+                    if (e.WasClosed) {
+                        parent.Activated -= handler;
+                        deactivator.Deactivated -= handler2;
+                    }
+                };
+                deactivator.Deactivated += handler2;
+            }
+        }
+
+        ///<summary>
+        /// Deactivates a child whenever the specified parent is deactivated.
+        ///</summary>
+        ///<param name="child">The child to deactivate.</param>
+        ///<param name="parent">The parent whose deactivation triggers the child's deactivation.</param>
+        public static void DeactivateWith(this IDeactivate child, IDeactivate parent) {
+            EventHandler<DeactivationEventArgs> handler = null;
+            handler = (s, e) => {
+                child.Deactivate(e.WasClosed);
+                if (e.WasClosed)
+                    parent.Deactivated -= handler;
+            };
+            parent.Deactivated += handler;
+        }
+
+        ///<summary>
+        /// Activates and Deactivates a child whenever the specified parent is Activated or Deactivated.
+        ///</summary>
+        ///<param name="child">The child to activate/deactivate.</param>
+        ///<param name="parent">The parent whose activation/deactivation triggers the child's activation/deactivation.</param>
+        public static void ConductWith<TChild, TParent>(this TChild child, TParent parent) 
+            where TChild : IActivate, IDeactivate
+            where TParent : IActivate, IDeactivate
+        {
+            child.ActivateWith(parent);
+            child.DeactivateWith(parent);
         }
     }
 }
