2ea2fa19354b549bad8ca4bfa8d8fccb18af26bf
EisenbergEffect <none@none>
2011-04-02 15:30:00 -0400

    Ensured that SequentialResult always calls dispose on the enumerator.

--- src/Caliburn.Micro.Silverlight/SequentialResult.cs
+++ src/Caliburn.Micro.Silverlight/SequentialResult.cs
@@ -1,44 +1,38 @@
-﻿namespace Caliburn.Micro
-{
+﻿namespace Caliburn.Micro {
     using System;
     using System.Collections.Generic;
 
     /// <summary>
-    /// An implementation of <see cref="IResult"/> that enables sequential execution of multiple results.
+    ///   An implementation of <see cref = "IResult" /> that enables sequential execution of multiple results.
     /// </summary>
-    public class SequentialResult : IResult
-    {
+    public class SequentialResult : IResult {
         readonly IEnumerator<IResult> enumerator;
         ActionExecutionContext context;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SequentialResult"/> class.
+        ///   Initializes a new instance of the <see cref = "SequentialResult" /> class.
         /// </summary>
-        /// <param name="enumerator">The enumerator.</param>
-        public SequentialResult(IEnumerator<IResult> enumerator)
-        {
+        /// <param name = "enumerator">The enumerator.</param>
+        public SequentialResult(IEnumerator<IResult> enumerator) {
             this.enumerator = enumerator;
         }
 
         /// <summary>
-        /// Occurs when execution has completed.
+        ///   Occurs when execution has completed.
         /// </summary>
         public event EventHandler<ResultCompletionEventArgs> Completed = delegate { };
 
         /// <summary>
-        /// Executes the result using the specified context.
+        ///   Executes the result using the specified context.
         /// </summary>
-        /// <param name="context">The context.</param>
-        public void Execute(ActionExecutionContext context)
-        {
+        /// <param name = "context">The context.</param>
+        public void Execute(ActionExecutionContext context) {
             this.context = context;
             ChildCompleted(null, new ResultCompletionEventArgs());
         }
 
-        void ChildCompleted(object sender, ResultCompletionEventArgs args)
-        {
-            if(args.Error != null || args.WasCancelled)
-            {
+        void ChildCompleted(object sender, ResultCompletionEventArgs args) {
+            if(args.Error != null || args.WasCancelled) {
                 OnComplete(args.Error, args.WasCancelled);
                 return;
             }
@@ -48,28 +42,23 @@
             if(previous != null)
                 previous.Completed -= ChildCompleted;
 
-            bool moveNextSucceeded = false;
-            try
-            {
+            var moveNextSucceeded = false;
+            try {
                 moveNextSucceeded = enumerator.MoveNext();
             }
-            catch (Exception ex)
-            {
+            catch(Exception ex) {
                 OnComplete(ex, false);
                 return;
             }
 
-            if (moveNextSucceeded)
-            {
-                try
-                {
+            if(moveNextSucceeded) {
+                try {
                     var next = enumerator.Current;
                     IoC.BuildUp(next);
                     next.Completed += ChildCompleted;
                     next.Execute(context);
                 }
-                catch (Exception ex)
-                {
+                catch(Exception ex) {
                     OnComplete(ex, false);
                     return;
                 }
@@ -77,9 +66,9 @@
             else OnComplete(null, false);
         }
 
-        void OnComplete(Exception error, bool wasCancelled)
-        {
-            Completed(this, new ResultCompletionEventArgs { Error = error,  WasCancelled = wasCancelled });
+        void OnComplete(Exception error, bool wasCancelled) {
+            enumerator.Dispose();
+            Completed(this, new ResultCompletionEventArgs { Error = error, WasCancelled = wasCancelled });
         }
     }
 }
