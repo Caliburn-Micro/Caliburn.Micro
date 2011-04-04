b8d0c06b05434cd31cd5b2eed9dfc2773a80e19a
Ryan Cromwell <cromwellryan@hotmail.com>
2011-04-04 14:57:18 -0400

    Moved the package contents to package subdir of nuget to remove things that shouldn't have been included.
    --HG--
    rename : nuget/caliburn.micro.nuspec => nuget/package/caliburn.micro.nuspec
    rename : nuget/tools/NET40/bootstrapper.cs => nuget/package/tools/NET40/bootstrapper.cs
    rename : nuget/tools/SL40-WindowsPhone/bootstrapper.cs => nuget/package/tools/SL40-WindowsPhone/bootstrapper.cs
    rename : nuget/tools/SL40/bootstrapper.cs => nuget/package/tools/SL40/bootstrapper.cs

--- nuget/package/tools/install.ps1
+++ nuget/package/tools/install.ps1
@@ -5,22 +5,27 @@ function Get-ScriptDirectory {
   Split-Path $Invoc.MyCommand.Path
 }
 
-function get-content-path() {
+function get-content-path($contentRoot) {
   $moniker = $project.Properties.Item("TargetFrameworkMoniker").Value
   $frameworkname = new-object System.Runtime.Versioning.FrameworkName($moniker)
 
   $id = $frameworkname.Identifier
 
+  write-host $frameworkname.Identifier
+  write-host $frameworkname.Profile
+
   if($id -eq ".NETFramework") { $relative = "NET40" }
-  if($id -eq "Silverlight" -and $frameworkname -eq "WindowsPhone") { $relative = "SL40-WindowsPhone" }
-  if($id -eq "Silverlight" ) { $relative = "SL40" }
+  elseif($id -eq "Silverlight" -and $frameworkname.Profile -eq "WindowsPhone") { $relative = "SL40-WindowsPhone" }
+  elseif($id -eq "Silverlight" ) { $relative = "SL40" }
  
-  [System.IO.Path]::Combine($root, $relative)
+  [System.IO.Path]::Combine($contentRoot, $relative)
 }
  
-  $contentSource = get-content-path
+  $contentSource = get-content-path ($rootPath + "\tools")
 
-  ls $contentSource | foreach-object { $project.ProjectItems.AddFromFileCopy($_.FullName) }
+  ls $contentSource | foreach-object { 
+    $project.ProjectItems.AddFromFileCopy($_.FullName) 
+  }
 
   write-host "Added Bootstrapper.cs.  You'll want to update the namespace and add this to your App.xaml as described here: http://caliburnmicro.codeplex.com/wikipage?title=Basic%20Configuration%2c%20Actions%20and%20Conventions&referringTitle=Documentation"
 
@@ -28,4 +33,3 @@ function get-content-path() {
 # WPF4 will be NETFramework,Version=v4.0,Profile=Client 
 # SL4 will be Silverlight,Version=v4.0 
 # WP7 will be Silverlight,Version=v4.0,Profile=WindowsPhone
-
