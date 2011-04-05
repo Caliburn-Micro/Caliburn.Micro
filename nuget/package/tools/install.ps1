f03c816af3e94089cd55b806dc637be7db89708d
EisenbergEffect <none@none>
2011-04-04 21:02:42 -0400

    -Updated Nuget to 1.2 -Work on custom content for nuget package installation.

--- nuget/package/tools/install.ps1
+++ nuget/package/tools/install.ps1
@@ -22,13 +22,22 @@ function get-content-path($contentRoot) {
 }
  
   $contentSource = get-content-path ($rootPath + "\tools")
-
-  ls $contentSource | foreach-object { 
-    $project.ProjectItems.AddFromFileCopy($_.FullName) 
+  $defaultNamespace = ""
+  
+  $project.Properties | foreach-object { 
+    if($_.Name -eq "DefaultNamespace"){
+    	$defaultNamespace = $_.Value
+    }
   }
 
-  write-host "Added Bootstrapper.cs.  You'll want to update the namespace and add this to your App.xaml as described here: http://caliburnmicro.codeplex.com/wikipage?title=Basic%20Configuration%2c%20Actions%20and%20Conventions&referringTitle=Documentation"
+  ls $contentSource | foreach-object { 
+  	$content = [System.IO.File]::ReadAllText($_.FullName)
+  	$content = $content.Replace("safeprojectname", $defaultNamespace)
+  	$content | out-file $_.FullName
+  	$project.ProjectItems.AddFromFileCopy($_.FullName)
+  }
 
+  write-host "Added Bootstrapper and Shell.  You will want to update the your App.xaml as described here: http://caliburnmicro.codeplex.com/wikipage?title=Basic%20Configuration%2c%20Actions%20and%20Conventions&referringTitle=Documentation"
  
 # WPF4 will be NETFramework,Version=v4.0,Profile=Client 
 # SL4 will be Silverlight,Version=v4.0 
