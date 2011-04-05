a2257872403d401cc0dff56ce36b06c28c31dd47
EisenbergEffect <none@none>
2011-04-05 11:53:37 -0400

    Removing some vestigial code from the nuget install script.

--- nuget/package/tools/install.ps1
+++ nuget/package/tools/install.ps1
@@ -1,10 +1,5 @@
 param($rootPath, $toolsPath, $package, $project)
 
-function Get-ScriptDirectory {
-	$Invoc = (Get-Variable MyInvocation -Scope 1).Value
-	Split-Path $Invoc.MyCommand.Path
-}
-
 function get-content-path($contentRoot) {
 	$moniker = $project.Properties.Item("TargetFrameworkMoniker").Value
 	$frameworkname = new-object System.Runtime.Versioning.FrameworkName($moniker)
@@ -18,7 +13,7 @@ function get-content-path($contentRoot) {
 	[System.IO.Path]::Combine($contentRoot, $relative)
 }
  
-$contentSource = get-content-path ($rootPath + "\tools")
+$contentSource = get-content-path($rootPath + "\tools")
 $defaultNamespace = $project.Properties.Item("DefaultNamespace").Value
 
 ls $contentSource | foreach-object { 
@@ -29,7 +24,3 @@ ls $contentSource | foreach-object {
 }
 
 write-host "Added Bootstrapper and Shell.  You will want to update the your App.xaml as described here: http://caliburnmicro.codeplex.com/wikipage?title=Basic%20Configuration%2c%20Actions%20and%20Conventions&referringTitle=Documentation"
- 
-# WPF4 will be NETFramework,Version=v4.0,Profile=Client 
-# SL4 will be Silverlight,Version=v4.0 
-# WP7 will be Silverlight,Version=v4.0,Profile=WindowsPhone
