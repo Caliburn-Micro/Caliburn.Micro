db7e0bdf8237384691cf8f9d36ffa9aeb309b4c5
EisenbergEffect <none@none>
2011-04-04 21:20:41 -0400

    Started work on WP7 Nuget custom content.

---
+++ nuget/package/tools/SL40-WindowsPhone/AppBootstrapper.cs
@@ -0,0 +1,78 @@
+﻿namespace safeprojectname {
+    using System;
+    using System.Collections.Generic;
+    using System.Windows.Controls;
+    using Framework;
+    using Microsoft.Phone.Controls;
+    using Caliburn.Micro;
+
+    public class AppBootstrapper : PhoneBootstrapper
+    {
+        PhoneContainer container;
+
+        protected override void Configure()
+        {
+            container = new PhoneContainer(this);
+
+            container.RegisterPerRequest(typeof(MainPageViewModel), "MainPageViewModel", typeof(MainPageViewModel));
+
+            container.RegisterInstance(typeof(INavigationService), null, new FrameAdapter(RootFrame));
+            container.RegisterInstance(typeof(IPhoneService), null, new PhoneApplicationServiceAdapter(PhoneService));
+
+            //container.Activator.InstallChooser<PhoneNumberChooserTask, PhoneNumberResult>();
+            //container.Activator.InstallLauncher<EmailComposeTask>();
+
+            AddCustomConventions();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+
+        static void AddCustomConventions()
+        {
+            ConventionManager.AddElementConvention<Pivot>(Pivot.ItemsSourceProperty, "SelectedItem", "SelectionChanged").ApplyBinding =
+                (viewModelType, path, property, element, convention) => {
+                    if (ConventionManager
+                        .GetElementConvention(typeof(ItemsControl))
+                        .ApplyBinding(viewModelType, path, property, element, convention))
+                    {
+                        ConventionManager
+                            .ConfigureSelectedItem(element, Pivot.SelectedItemProperty, viewModelType, path);
+                        ConventionManager
+                            .ApplyHeaderTemplate(element, Pivot.HeaderTemplateProperty, viewModelType);
+                        return true;
+                    }
+
+                    return false;
+                };
+
+            ConventionManager.AddElementConvention<Panorama>(Panorama.ItemsSourceProperty, "SelectedItem", "SelectionChanged").ApplyBinding =
+                (viewModelType, path, property, element, convention) => {
+                    if (ConventionManager
+                        .GetElementConvention(typeof(ItemsControl))
+                        .ApplyBinding(viewModelType, path, property, element, convention))
+                    {
+                        ConventionManager
+                            .ConfigureSelectedItem(element, Panorama.SelectedItemProperty, viewModelType, path);
+                        ConventionManager
+                            .ApplyHeaderTemplate(element, Panorama.HeaderTemplateProperty, viewModelType);
+                        return true;
+                    }
+
+                    return false;
+                };
+        }
+    }
+}
