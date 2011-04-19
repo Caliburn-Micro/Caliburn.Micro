4da91bda9295b75d72d9bb43d827198100b787b6
EisenbergEffect <none@none>
2011-04-18 21:56:21 -0400

    Upping version number in assembly metadata in preparation for new work.

--- src/Caliburn.Micro.Silverlight/Properties/AssemblyInfo.cs
+++ src/Caliburn.Micro.Silverlight/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Windows.Markup;
 [assembly: AssemblyCopyright("Copyright © 2010")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6449e9cb-4d4d-4d79-8f73-71a2fc647109")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.Micro")]
 [assembly: XmlnsPrefix("http://www.caliburnproject.org", "cal")]
