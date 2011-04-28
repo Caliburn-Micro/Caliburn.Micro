debff04d8dafaa88c6aba2818bdc102d9c4ae0d9
EisenbergEffect <none@none>
2011-04-27 21:30:21 -0400

    Added a "possibly" temporary assembly for work on the new launcher/chooser mechanism and tombstoning support. DO NOT USE THIS!

---
+++ src/Caliburn.Micro.WP7.Extensions/ContainerExtensions.cs
@@ -0,0 +1,85 @@
+﻿namespace Caliburn.Micro {
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Microsoft.Phone.Controls;
+
+    public static class ContainerExtensions {
+        public static SimpleContainer Singleton<TImplementation>(this SimpleContainer container) {
+            container.RegisterSingleton(typeof(TImplementation), null, typeof(TImplementation));
+            return container;
+        }
+
+        public static SimpleContainer Singleton<TService, TImplementation>(this SimpleContainer container)
+            where TImplementation : TService {
+            container.RegisterSingleton(typeof(TService), null, typeof(TImplementation));
+            return container;
+        }
+
+        public static SimpleContainer PerRequest<TService, TImplementation>(this SimpleContainer container)
+            where TImplementation : TService {
+            container.RegisterPerRequest(typeof(TService), null, typeof(TImplementation));
+            return container;
+        }
+
+        public static SimpleContainer PerRequest<TImplementation>(this SimpleContainer container) {
+            container.RegisterPerRequest(typeof(TImplementation), null, typeof(TImplementation));
+            return container;
+        }
+
+        public static SimpleContainer Instance<TService>(this SimpleContainer container, TService instance) {
+            container.RegisterInstance(typeof(TService), null, instance);
+            return container;
+        }
+
+        public static SimpleContainer Handler<TService>(this SimpleContainer container, Func<object> handler) {
+            container.RegisterHandler(typeof(TService), null, handler);
+            return container;
+        }
+
+        public static void RegisterPerRequestPageVM<TViewModel>(this SimpleContainer container) {
+            container.RegisterPerRequestPageVM(typeof(TViewModel));
+        }
+
+        public static void RegisterPerRequestPageVM(this SimpleContainer container, Type viewModelType) {
+            container.RegisterPerRequest(viewModelType, viewModelType.Name, viewModelType);
+        }
+
+        public static SimpleContainer AllTypesOf<TService>(this SimpleContainer container, Assembly assembly, Func<Type, bool> filter = null) {
+            if(filter == null)
+                filter = type => true;
+
+            var types = from type in assembly.GetTypes()
+                        where typeof(TService).IsAssignableFrom(type)
+                              && !type.IsAbstract
+                              && !type.IsInterface
+                              && filter(type)
+                        select type;
+
+            foreach(var type in types) {
+                container.RegisterSingleton(typeof(TService), null, type);
+            }
+
+            return container;
+        }
+
+        public static void RegisterAllViewModelsForPages(this SimpleContainer container) {
+            var pages = from assembly in AssemblySource.Instance
+                        from type in assembly.GetTypes()
+                        where typeof(PhoneApplicationPage).IsAssignableFrom(type)
+                        select type;
+
+            foreach(var page in pages) {
+                var key = ViewModelLocator.DetermineKeyForViewType(page);
+
+                var viewModelType = (from assembly in AssemblySource.Instance
+                                     from type in assembly.GetTypes()
+                                     where type.Name == key
+                                     select type).FirstOrDefault();
+
+                if(viewModelType != null)
+                    container.RegisterPerRequestPageVM(viewModelType);
+            }
+        }
+    }
+}
---
+++ src/Caliburn.Micro.WP7.Extensions/PhoneContainer.cs
@@ -0,0 +1,30 @@
+﻿namespace Caliburn.Micro {
+    public class PhoneContainer : SimpleContainer {
+        readonly PhoneBootstrapper bootstrapper;
+
+        public PhoneContainer(PhoneBootstrapper bootstrapper) {
+            this.bootstrapper = bootstrapper;
+        }
+
+        public void RegisterWithPhoneService<TService, TImplementation>() where TImplementation : TService {
+            RegisterHandler(typeof(TService), null, () => {
+                var phoneService = (IPhoneService)GetInstance(typeof(IPhoneService), null);
+
+                if(phoneService.State.ContainsKey(typeof(TService).FullName))
+                    return phoneService.State[typeof(TService).FullName];
+
+                var instance = BuildInstance(typeof(TImplementation));
+                phoneService.State[typeof(TService).FullName] = instance;
+
+                return instance;
+            });
+        }
+
+        public void RegisterPhoneServices(bool treatViewAsLoaded = false) {
+            RegisterInstance(typeof(INavigationService), null, new FrameAdapter(bootstrapper.RootFrame, treatViewAsLoaded));
+            RegisterInstance(typeof(IPhoneService), null, new PhoneApplicationServiceAdapter(bootstrapper.PhoneService));
+            RegisterSingleton(typeof(IWindowManager), null, typeof(WindowManager));
+            RegisterSingleton(typeof(IEventAggregator), null, typeof(EventAggregator));
+        }
+    }
+}
