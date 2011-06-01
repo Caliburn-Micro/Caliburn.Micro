29beae3948b43cbdcfefb60aede9dfc128c8c712
EisenbergEffect <none@none>
2011-06-01 11:54:40 -0400

    Fixed up the assembly metadata for the extensions assembly.

--- src/Caliburn.Micro.WP7.Extensions/Properties/AssemblyInfo.cs
+++ src/Caliburn.Micro.WP7.Extensions/Properties/AssemblyInfo.cs
@@ -1,35 +1,20 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Caliburn.Micro.WP7.Extensions")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Caliburn.Micro.WP7.Extensions")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyTitle("Caliburn.Micro.Extensions")]
+[assembly: AssemblyDescription("Extensions to the core Caliburn.Micro framework.")]
+[assembly: AssemblyProduct("Caliburn.Micro.Extensions")]
+[assembly: AssemblyCompany("Blue Spire Consulting, Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2010")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("76ae9648-d7f5-495b-b59f-b3bc2bd0a579")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.Micro")]
+[assembly: XmlnsPrefix("http://www.caliburnproject.org", "cal")]
+
+#if !SILVERLIGHT
+[assembly: CLSCompliant(true)]
+#endif
