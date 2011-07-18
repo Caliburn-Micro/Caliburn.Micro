4c3dfa6261273ed09d58d0e8ab928555615d7f60
EisenbergEffect <none@none>
2011-07-18 17:14:48 -0400

    Added a simple project which can build child "feature" packages for me easily.

---
+++ src/Caliburn.Micro.PackageBuilder/PackageList.cs
@@ -0,0 +1,35 @@
+﻿namespace Caliburn.Micro.PackageBuilder {
+    using System.Collections.Generic;
+
+    public class PackageList : List<PackageModel> {
+        public PackageList() {
+            Add(new PackageModel {
+                Id = "Caliburn.Micro.Container",
+                Description = "A simple IoC Container for .NET, Silverlight and WP7.",
+                Tags = "Silverlight WPF WP7 SL4 SL Phone IoC DI Container",
+                Content = {
+                    "../../../Caliburn.Micro.WP7.Extensions/ContainerExtensions.cs",
+                    "../../../Caliburn.Micro.WP7.Extensions/SimpleContainer.cs"
+                }
+            });
+
+            Add(new PackageModel {
+                Id = "Caliburn.Micro.EventAggregator",
+                Description = "A small, simple event aggregator implementation for .NET, Silverlight and WP7.",
+                Tags = "Silverlight WPF WP7 SL4 SL Phone EA PubSub EventAggregator Messaging Messenger Publish Subscribe Event",
+                Content = {
+                    "../../../Caliburn.Micro.Silverlight/EventAggregator.cs",
+                }
+            });
+
+            Add(new PackageModel {
+                Id = "Caliburn.Micro.INPC",
+                Description = "A standalone version of Caliburn.Micro's INPC implementation, BindableCollection, Execute.OnUIThread and InDesignMode checking.",
+                Tags = "Silverlight WPF WP7 SL4 SL Phone INPC INotifyPropertyChanged MVVM",
+                Content = {
+                    "../../../Caliburn.Micro.Silverlight/INPC.cs",
+                }
+            });
+        }
+    }
+}
---
+++ src/Caliburn.Micro.PackageBuilder/PackageModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Caliburn.Micro.PackageBuilder {
+    using System.Collections.Generic;
+
+    public class PackageModel {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string Tags { get; set; }
+
+        public List<FrameworkAssembly> FrameworkAssemblies { get; set; }
+        public List<string> Content { get; set; }
+
+        public PackageModel() {
+            FrameworkAssemblies = new List<FrameworkAssembly>();
+            Content = new List<string>();
+        }
+    }
+}
