4f4a94d8f946f894d868add1918aa4a2e9c42372
EisenbergEffect <none@none>
2011-10-14 08:42:19 -0400

    -Updated some more metadata -Updated the package builder

--- src/Caliburn.Micro.PackageBuilder/PackageList.cs
+++ src/Caliburn.Micro.PackageBuilder/PackageList.cs
@@ -5,8 +5,8 @@
         public PackageList() {
             Add(new PackageModel {
                 Id = "Caliburn.Micro.Container",
-                Description = "A simple IoC Container for .NET, Silverlight, WP7, Mono and Unity3d.",
-                Tags = "Silverlight WPF WP7 SL4 SL Phone IoC DI Container Mono Unity3d",
+                Description = "A simple IoC Container for .NET, SL, WP7, WinRT, Mono and Unity3d.",
+                Tags = "Silverlight WPF WP7 WP71 SL4 SL Phone IoC DI Container Mono Unity3d WinRT",
                 Content = {
                     "../../../Caliburn.Micro.Silverlight/ExtensionMethods.cs",
                     "../../../Caliburn.Micro.WP7.Extensions/ContainerExtensions.cs",
@@ -16,8 +16,8 @@
 
             Add(new PackageModel {
                 Id = "Caliburn.Micro.EventAggregator",
-                Description = "A small, simple event aggregator implementation for .NET, Silverlight, WP7, Mono and Unity3d.",
-                Tags = "Silverlight WPF WP7 SL4 SL Phone EA PubSub EventAggregator Messaging Messenger Publish Subscribe Event Mono Unity3d",
+                Description = "A small, simple event aggregator implementation for .NET, SL, WP7, WinRT, Mono and Unity3d.",
+                Tags = "Silverlight WPF WP7 WP71 SL4 SL Phone EA PubSub EventAggregator Messaging Messenger Publish Subscribe Event Mono Unity3d WinRT",
                 Content = {
                     "../../../Caliburn.Micro.Silverlight/ExtensionMethods.cs",
                     "../../../Caliburn.Micro.Silverlight/EventAggregator.cs",
@@ -27,7 +27,7 @@
             Add(new PackageModel {
                 Id = "Caliburn.Micro.INPC",
                 Description = "A standalone version of Caliburn.Micro's INPC implementation, BindableCollection, Execute.OnUIThread and InDesignMode checking.",
-                Tags = "Silverlight WPF WP7 SL4 SL Phone INPC INotifyPropertyChanged MVVM",
+                Tags = "Silverlight WPF WP7 WP71 SL4 SL Phone INPC INotifyPropertyChanged MVVM",
                 Content = {
                     "../../../Caliburn.Micro.Silverlight/ExtensionMethods.cs",
                     "../../../Caliburn.Micro.Silverlight/INPC.cs",
