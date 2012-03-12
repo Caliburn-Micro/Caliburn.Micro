be5040686b7cd8c5d433fcaad63a62cb906bf3e6
EisenbergEffect <none@none>
2012-03-12 17:34:15 -0400

    Updating metadta to 1.3.2 version number.

--- src/Caliburn.Micro.SL5/Properties/AssemblyInfo.cs
+++ src/Caliburn.Micro.SL5/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Windows.Markup;
 [assembly: AssemblyCopyright("Copyright © 2010")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9d606d8d-23a3-4965-8318-558fb10724c9")]
-[assembly: AssemblyVersion("1.3.1")]
-[assembly: AssemblyFileVersion("1.3.1")]
+[assembly: AssemblyVersion("1.3.2")]
+[assembly: AssemblyFileVersion("1.3.2")]
 
 [assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.Micro")]
 [assembly: XmlnsPrefix("http://www.caliburnproject.org", "cal")]
