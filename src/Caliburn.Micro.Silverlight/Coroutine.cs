f503caed685517d3e6e249199e901d0513583652
EisenbergEffect <none@none>
2012-07-23 16:46:26 -0400

    Some code formatting and name changes for consistency.

--- src/Caliburn.Micro.Silverlight/Coroutine.cs
+++ src/Caliburn.Micro.Silverlight/Coroutine.cs
@@ -53,12 +53,11 @@
     }
 
     /// <summary>
-    ///  Denoes a clas which can handle a particular type of message using a Coroutine.
+    ///  Denotes a class which can handle a particular type of message and uses a Coroutine to do so.
     /// </summary>
-    public interface IHandleAsCoroutine<TMessage> : IHandle
-    {
+    public interface IHandleWithCoroutine<TMessage> : IHandle {
 		/// <summary>
-		///  Handle the message as a Coroutine.
+		///  Handle the message with a Coroutine.
 		/// </summary>
 		/// <param name="message">The message.</param>
 		/// <returns>The coroutine to execute.</returns>
