cecda3b9da908877b653c2bd74d15333d47fbeb4
Matt@ZENBOOK.clarityinternal.net <Matt@ZENBOOK.clarityinternal.net>
2012-11-05 16:38:35 -0600

    - added WP8 support to VS2012 sln

---
+++ src/Caliburn.Micro.WP8/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle("Caliburn Micro")]
+[assembly: AssemblyDescription("A small, yet powerful framework designed for Xaml platforms, Caliburn.Micro implements a variety of UI patterns for solving real-world problems. Patterns that are highlighted include MVVM (Presentation Model), MVP and MVC.")]
+[assembly: AssemblyProduct("Caliburn.Micro")]
+[assembly: AssemblyCompany("Blue Spire Consulting, Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2010")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b7b4ec5c-5959-44da-b410-44695cadc0a6")]
+[assembly: AssemblyVersion("1.3.2")]
+[assembly: AssemblyFileVersion("1.3.2")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.Micro")]
+[assembly: XmlnsPrefix("http://www.caliburnproject.org", "cal")]
+
+[assembly: CLSCompliant(true)]
