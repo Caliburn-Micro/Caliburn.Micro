fc148b8dceac6e9827bf098f4dcfa07cb9e62333
Nigel Sampson <nigel.sampson@compiledexperience.com>
2013-08-06 21:48:13 +1200

    New projects

---
+++ src/Caliburn.Micro.WinRT81/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+//using System.Windows.Markup;
+
+[assembly: AssemblyTitle("Caliburn Micro")]
+[assembly: AssemblyDescription("A small, yet powerful framework designed for Xaml platforms, Caliburn.Micro implements a variety of UI patterns for solving real-world problems. Patterns that are highlighted include MVVM (Presentation Model), MVP and MVC.")]
+
+//[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("566D4CAA-177A-4ED0-94F6-6367871DB4A5")]
+
+//[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.Micro")]
+//[assembly: XmlnsPrefix("http://www.caliburnproject.org", "cal")]
