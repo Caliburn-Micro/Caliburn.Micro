1ebb84c69c19f18edb93259652385561ca41c15b
tibel <tibel@codeplex.fake>
2013-09-21 20:43:15 +0200

    change version to 1.6 and add WinRT81 assemblies to nuget package

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Resources;
 
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("1.5.3")]
-[assembly: AssemblyFileVersion("1.5.3")]
+[assembly: AssemblyVersion("1.6.0")]
+[assembly: AssemblyFileVersion("1.6.0")]
