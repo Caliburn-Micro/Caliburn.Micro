3c30ddde62884339ccee67ff0aacddeed1acf247
tibel <tibel@codeplex.fake>
2013-10-03 22:16:49 +0200

    port EventAggregator

--- src/Caliburn.Micro/EventAggregator.cs
+++ src/Caliburn.Micro/EventAggregator.cs
@@ -1,87 +1,17 @@
-﻿#if NETFX_CORE && !WinRT
-#define WinRT
-#endif
-
-namespace Caliburn.Micro {
+﻿namespace Caliburn.Micro {
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
 
     /// <summary>
-    ///   A marker interface for classes that subscribe to messages.
-    /// </summary>
-    public interface IHandle { }
-
-    /// <summary>
-    ///   Denotes a class which can handle a particular type of message.
-    /// </summary>
-    /// <typeparam name = "TMessage">The type of message to handle.</typeparam>
-    public interface IHandle<TMessage> : IHandle {  //don't use contravariance here
-        /// <summary>
-        ///   Handles the message.
-        /// </summary>
-        /// <param name = "message">The message.</param>
-        void Handle(TMessage message);
-    }
-
-    /// <summary>
-    ///   Enables loosely-coupled publication of and subscription to events.
-    /// </summary>
-    public interface IEventAggregator {
-        /// <summary>
-        ///   Gets or sets the default publication thread marshaller.
-        /// </summary>
-        /// <value>
-        ///   The default publication thread marshaller.
-        /// </value>
-        Action<System.Action> PublicationThreadMarshaller { get; set; }
-
-        /// <summary>
-        /// Searches the subscribed handlers to check if we have a handler for
-        /// the message type supplied.
-        /// </summary>
-        /// <param name="messageType">The message type to check with</param>
-        /// <returns>True if any handler is found, false if not.</returns>
-        bool HandlerExistsFor(Type messageType);
-
-        /// <summary>
-        ///   Subscribes an instance to all events declared through implementations of <see cref = "IHandle{T}" />
-        /// </summary>
-        /// <param name = "subscriber">The instance to subscribe for event publication.</param>
-        void Subscribe(object subscriber);
-
-        /// <summary>
-        ///   Unsubscribes the instance from all events.
-        /// </summary>
-        /// <param name = "subscriber">The instance to unsubscribe.</param>
-        void Unsubscribe(object subscriber);
-
-        /// <summary>
-        ///   Publishes a message.
-        /// </summary>
-        /// <param name = "message">The message instance.</param>
-        /// <remarks>
-        ///   Uses the default thread marshaller during publication.
-        /// </remarks>
-        void Publish(object message);
-
-        /// <summary>
-        ///   Publishes a message.
-        /// </summary>
-        /// <param name = "message">The message instance.</param>
-        /// <param name = "marshal">Allows the publisher to provide a custom thread marshaller for the message publication.</param>
-        void Publish(object message, Action<System.Action> marshal);
-    }
-
-    /// <summary>
-    ///   Enables loosely-coupled publication of and subscription to events.
+    /// Enables loosely-coupled publication of and subscription to events.
     /// </summary>
     public class EventAggregator : IEventAggregator {
         readonly List<Handler> handlers = new List<Handler>();
 
         /// <summary>
-        ///   The default thread marshaller used for publication;
+        /// The default thread marshaller used for publication;
         /// </summary>
         public static Action<System.Action> DefaultPublicationThreadMarshaller = action => action();
 
@@ -91,18 +21,15 @@ namespace Caliburn.Micro {
         public static Action<object, object> HandlerResultProcessing = (target, result) => { };
 
         /// <summary>
-        ///   Initializes a new instance of the <see cref = "EventAggregator" /> class.
+        /// Initializes a new instance of the <see cref = "EventAggregator" /> class.
         /// </summary>
         public EventAggregator() {
             PublicationThreadMarshaller = DefaultPublicationThreadMarshaller;
         }
 
         /// <summary>
-        ///   Gets or sets the default publication thread marshaller.
+        /// Gets or sets the default publication thread marshaller.
         /// </summary>
-        /// <value>
-        ///   The default publication thread marshaller.
-        /// </value>
         public Action<System.Action> PublicationThreadMarshaller { get; set; }
 
         /// <summary>
@@ -112,11 +39,11 @@ namespace Caliburn.Micro {
         /// <param name="messageType">The message type to check with</param>
         /// <returns>True if any handler is found, false if not.</returns>
         public bool HandlerExistsFor(Type messageType) {
-                return handlers.Any(handler => handler.Handles(messageType) & !handler.IsDead);
+            return handlers.Any(handler => handler.Handles(messageType) & !handler.IsDead);
         }
 
         /// <summary>
-        ///   Subscribes an instance to all events declared through implementations of <see cref = "IHandle{T}" />
+        /// Subscribes an instance to all events declared through implementations of <see cref = "IHandle{T}" />
         /// </summary>
         /// <param name = "subscriber">The instance to subscribe for event publication.</param>
         public virtual void Subscribe(object subscriber) {
@@ -133,7 +60,7 @@ namespace Caliburn.Micro {
         }
 
         /// <summary>
-        ///   Unsubscribes the instance from all events.
+        /// Unsubscribes the instance from all events.
         /// </summary>
         /// <param name = "subscriber">The instance to unsubscribe.</param>
         public virtual void Unsubscribe(object subscriber) {
@@ -150,7 +77,7 @@ namespace Caliburn.Micro {
         }
 
         /// <summary>
-        ///   Publishes a message.
+        /// Publishes a message.
         /// </summary>
         /// <param name = "message">The message instance.</param>
         /// <remarks>
@@ -164,7 +91,7 @@ namespace Caliburn.Micro {
         }
 
         /// <summary>
-        ///   Publishes a message.
+        /// Publishes a message.
         /// </summary>
         /// <param name = "message">The message instance.</param>
         /// <param name = "marshal">Allows the publisher to provide a custom thread marshaller for the message publication.</param>
@@ -207,17 +134,6 @@ namespace Caliburn.Micro {
             public Handler(object handler) {
                 reference = new WeakReference(handler);
 
-#if WinRT
-                var handlerInfo = typeof(IHandle).GetTypeInfo();
-                var interfaces = handler.GetType().GetTypeInfo().ImplementedInterfaces
-                    .Where(x => handlerInfo.IsAssignableFrom(x.GetTypeInfo()) && x.GetTypeInfo().IsGenericType);
-
-                foreach (var @interface in interfaces) {
-                    var type = @interface.GenericTypeArguments[0];
-                    var method = @interface.GetTypeInfo().DeclaredMethods.First(x => x.Name == "Handle");
-                    supportedHandlers[type] = method;
-                }
-#else
                 var interfaces = handler.GetType().GetInterfaces()
                     .Where(x => typeof(IHandle).IsAssignableFrom(x) && x.IsGenericType);
 
@@ -226,7 +142,6 @@ namespace Caliburn.Micro {
                     var method = @interface.GetMethod("Handle");
                     supportedHandlers[type] = method;
                 }
-#endif
             }
 
             public bool Matches(object instance) {
