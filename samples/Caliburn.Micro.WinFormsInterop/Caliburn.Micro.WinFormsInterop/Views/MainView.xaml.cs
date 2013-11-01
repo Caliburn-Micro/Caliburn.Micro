4fb33c49fc66b6b423a077e9f690860406354b83
tibel <tibel@codeplex.fake>
2013-11-01 21:33:45 +0100

    add WinFormsInterop sample

---
+++ samples/Caliburn.Micro.WinFormsInterop/Caliburn.Micro.WinFormsInterop/Views/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Caliburn.Micro.WinFormsInterop.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
