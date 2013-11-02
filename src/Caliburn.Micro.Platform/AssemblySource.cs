4cec3170ad3cc4a2fbfb008f55af8194f87fb0d8
tibel <tibel@codeplex.fake>
2013-11-02 18:50:14 +0100

    View/ViewModel-Locator performance improvement #342

--- src/Caliburn.Micro.Platform/AssemblySource.cs
+++ src/Caliburn.Micro.Platform/AssemblySource.cs
@@ -1,8 +1,15 @@
 ﻿namespace Caliburn.Micro {
     using System;
     using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.ComponentModel;
     using System.Linq;
     using System.Reflection;
+#if !WinRT
+    using System.Windows;
+#else
+    using Windows.UI.Xaml;
+#endif
 
     /// <summary>
     /// A source of assemblies that are inspectable by the framework.
@@ -13,22 +20,47 @@
         /// </summary>
         public static readonly IObservableCollection<Assembly> Instance = new BindableCollection<Assembly>();
 
+        private static readonly IDictionary<String, Type> TypeNameCache = new Dictionary<string, Type>();
+
+        static AssemblySource() {
+            Instance.CollectionChanged += (s, e) => {
+                switch (e.Action) {
+                    case NotifyCollectionChangedAction.Add:
+                        e.NewItems.OfType<Assembly>()
+                            .SelectMany(a => ExtractTypes(a))
+                            .Apply(t => TypeNameCache.Add(t.FullName, t));
+                        break;
+                    case NotifyCollectionChangedAction.Remove:
+                    case NotifyCollectionChangedAction.Replace:
+                    case NotifyCollectionChangedAction.Reset:
+                        TypeNameCache.Clear();
+                        Instance
+                            .SelectMany(a => ExtractTypes(a))
+                            .Apply(t => TypeNameCache.Add(t.FullName, t));
+                        break;
+                }
+            };
+        }
+
         /// <summary>
-        ///   Finds a type which matches a sequence or one of the elements in the sequence of names.
+        /// Extracts the types from the spezified assembly for <see cref="FindTypeByNames"/>.
         /// </summary>
-        /// <remarks>
-        ///   By default, the first name which matches a <see cref="Type.FullName"/> from the GetExportedTypes 
-        ///   of assemblies in <see cref="AssemblySource.Instance"/> is selected.
-        /// </remarks>
-        public static Func<IEnumerable<string>, Type> FindTypeByNames = names => {
+        public static Func<Assembly, IEnumerable<Type>> ExtractTypes = assembly =>
+            assembly.GetExportedTypes()
+                .Where(t =>
+                    typeof (UIElement).IsAssignableFrom(t) ||
+                    typeof (INotifyPropertyChanged).IsAssignableFrom(t));
+
+        /// <summary>
+        /// Finds a type which matches one of the elements in the sequence of names.
+        /// </summary>
+        public static Type FindTypeByNames(IEnumerable<string> names) {
             if (names == null) {
                 return null;
             }
 
-            var type = names
-                .Join(Instance.SelectMany(a => a.GetExportedTypes()), n => n, t => t.FullName, (n, t) => t)
-                .FirstOrDefault();
+            var type = names.Select(n => TypeNameCache.GetValueOrDefault(n)).FirstOrDefault(t => t != null);
             return type;
-        };
+        }
     }
 }
--- src/Caliburn.Micro.Platform/ExtensionMethods.cs
+++ src/Caliburn.Micro.Platform/ExtensionMethods.cs
@@ -2,7 +2,6 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Linq.Expressions;
     using System.Reflection;
 
     /// <summary>
@@ -57,5 +56,18 @@
             return target.GetTypeInfo().IsAssignableFrom(type.GetTypeInfo());
         }
 #endif
+
+        /// <summary>
+        /// Gets the value for a key. If the key does not exist, return default(TValue);
+        /// </summary>
+        /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+        /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+        /// <param name="dictionary">The dictionary to call this method on.</param>
+        /// <param name="key">The key to look up.</param>
+        /// <returns>The key value. default(TValue) if this key is not in the dictionary.</returns>
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key) {
+            TValue result;
+            return dictionary.TryGetValue(key, out result) ? result : default(TValue);
+        }
     }
 }
