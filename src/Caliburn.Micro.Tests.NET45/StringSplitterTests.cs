cbbc2ad8d2757cb82e0b798aa831aed61e83a6a1
Fredrik Leijon <fredrik.leijon@gmail.com>
2014-01-31 11:29:02 +0100

    Extracted StringSplitting from Parser and added tests for it

---
+++ src/Caliburn.Micro.Tests.NET45/StringSplitterTests.cs
@@ -0,0 +1,80 @@
+﻿namespace Caliburn.Micro.WPF.Tests
+{
+
+    using Xunit;
+
+    public class StringSplitterTests
+    {
+
+        [Fact]
+        public void ShouldSplitAsNormalWhenStringContainsNoBrackets()
+        {
+            var message = "message string for testing";
+            var separator = ' ';
+
+            var strings = StringSplitter.Split(message, separator);
+
+            Assert.Equal(4, strings.Length);
+        }
+
+        [Fact]
+        public void ShouldNotSplitValuesWithinBrackets()
+        {
+            var message = "[message string] for testing";
+            var separator = ' ';
+
+            var strings = StringSplitter.Split(message, separator);
+
+            Assert.Equal(3, strings.Length);
+        }
+
+        [Fact]
+        public void ShouldSplitParametersOnComman()
+        {
+            var message = "message, string, for, testing";
+
+            var strings = StringSplitter.SplitParameters(message);
+
+            Assert.Equal(4, strings.Length);
+        }
+
+        [Fact]
+        public void ShouldNotSplitStringsInsideQuotes() {
+            var message = "message, \"string, for\", testing";
+
+            var strings = StringSplitter.SplitParameters(message);
+
+            Assert.Equal(3, strings.Length);
+        }
+
+        [Fact]
+        public void ShouldNotSplitStringsInsideCurlyBraces()
+        {
+            var message = "message, {string, for}, testing";
+
+            var strings = StringSplitter.SplitParameters(message);
+
+            Assert.Equal(3, strings.Length);
+        }
+
+        [Fact]
+        public void ShouldNotSplitStringsInsideSquareBrackets()
+        {
+            var message = "message, [string, for], testing";
+
+            var strings = StringSplitter.SplitParameters(message);
+
+            Assert.Equal(3, strings.Length);
+        }
+
+        [Fact]
+        public void ShouldNotSplitStringsInsideParenthesis()
+        {
+            var message = "message, (string, for), testing";
+
+            var strings = StringSplitter.SplitParameters(message);
+
+            Assert.Equal(3, strings.Length);
+        }
+    }
+}
