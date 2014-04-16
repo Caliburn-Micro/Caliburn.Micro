14d992888a24e97f581a4701b52b45b33451d49d
Nigel Sampson <nigel.sampson@compiledexperience.com>
2014-04-16 16:16:07 +1200

    Build nuget symbol packages

--- nuget/create-packages.cmd
+++ nuget/create-packages.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
 del *.nupkg
-.\nuget.exe pack .\Caliburn.Micro.Core.nuspec
-.\nuget.exe pack .\Caliburn.Micro.nuspec
+.\nuget.exe pack .\Caliburn.Micro.Core.nuspec -symbols
+.\nuget.exe pack .\Caliburn.Micro.nuspec -symbols
 .\nuget.exe pack .\Caliburn.Micro.Start.nuspec
