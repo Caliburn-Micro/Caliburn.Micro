dba9bd17399829b994c3cee5fd0afa42d6ad00a0
Thomas Ibel <tibel@users.noreply.github.com>
2014-04-25 13:58:54 +0200

    add source to symbol packages

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Resources;
 
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("2.0.0.5")]
-[assembly: AssemblyFileVersion("2.0.0.5")]
+[assembly: AssemblyVersion("2.0.0.6")]
+[assembly: AssemblyFileVersion("2.0.0.6")]
