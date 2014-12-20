410d682da0bf8e4c2fc4ab0a6b4b9c3712657a97
Thomas Ibel <tibel@users.noreply.github.com>
2014-12-20 15:20:35 +0100

    add missing XML doc

--- src/Caliburn.Micro.Extensions.WIN8/SettingsWindowManager.cs
+++ src/Caliburn.Micro.Extensions.WIN8/SettingsWindowManager.cs
@@ -17,6 +17,7 @@
         /// <param name="viewModel">The settings view model.</param>
         /// <param name="commandLabel">The settings command label.</param>
         /// <param name="viewSettings">The optional dialog settings.</param>
+        /// <param name="independent">Show settings independent from <seealso cref="Windows.UI.ApplicationSettings.SettingsPane"/>.</param>
         public async void ShowSettingsFlyout(object viewModel, string commandLabel,
             IDictionary<string, object> viewSettings = null, bool independent = false) {
             var view = ViewLocator.LocateForModel(viewModel, null, null);
