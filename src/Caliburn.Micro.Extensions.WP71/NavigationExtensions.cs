e2eaf818794b54991b06522b4159d27af6d2aebe
Nigel Sampson <nigel.sampson@compiledexperience.com>
2015-06-18 10:52:56 +1200

    Rename UriFor to For. #176

--- src/Caliburn.Micro.Extensions.WP71/NavigationExtensions.cs
+++ src/Caliburn.Micro.Extensions.WP71/NavigationExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Caliburn.Micro {
+﻿using System;
+
+namespace Caliburn.Micro {
     /// <summary>
     /// Extension methods related to navigation.
     /// </summary>
@@ -9,7 +11,18 @@
         /// <typeparam name="TViewModel">The type of the view model.</typeparam>
         /// <param name="navigationService">The navigation service.</param>
         /// <returns>The builder.</returns>
+        [Obsolete("Use For instead of UriFor")]
         public static UriBuilder<TViewModel> UriFor<TViewModel>(this INavigationService navigationService) {
+            return navigationService.For<TViewModel>();
+        }
+
+        /// <summary>
+        /// Creates a Uri builder based on a view model type.
+        /// </summary>
+        /// <typeparam name="TViewModel">The type of the view model.</typeparam>
+        /// <param name="navigationService">The navigation service.</param>
+        /// <returns>The builder.</returns>
+        public static UriBuilder<TViewModel> For<TViewModel>(this INavigationService navigationService) {
             return new UriBuilder<TViewModel>().AttachTo(navigationService);
         }
     }
