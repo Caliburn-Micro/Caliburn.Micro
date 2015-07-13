5ebb1d59f854f4d4d5730deca7ecdfe73d2bd96a
Nigel Sampson <nigel.sampson@compiledexperience.com>
2015-07-13 14:52:25 +1200

    Migrate extensions into platform and update samples. #191

--- src/Caliburn.Micro.Platform/wp71/NavigationHelper.cs
+++ src/Caliburn.Micro.Platform/wp71/NavigationHelper.cs
@@ -46,11 +46,8 @@
             if (navigationService == null) {
                 throw new InvalidOperationException("Cannot navigate without attaching an INavigationService. Call AttachTo first.");
             }
-#if WinRT
-            navigationService.NavigateToViewModel<TViewModel>(uri.AbsoluteUri);
-#else
+
             navigationService.Navigate(uri);
-#endif
         }
 
         /// <summary>
@@ -65,12 +62,8 @@
 
             var packUri = ViewLocator.DeterminePackUriFromType(typeof(TViewModel), viewType);
             var qs = BuildQueryString();
-#if WinRT
-            // We need a value uri here otherwise there are problems using uri as a parameter
-            return new Uri("caliburn://" + packUri + qs, UriKind.Absolute);
-#else
+
             return new Uri(packUri + qs, UriKind.Relative);
-#endif
         }
 
         string BuildQueryString() {
