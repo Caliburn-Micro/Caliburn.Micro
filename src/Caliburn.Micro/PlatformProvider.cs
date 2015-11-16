574402541beb6c0e8483b0a74877d638d02c37b4
Nigel Sampson <nigel.sampson@compiledexperience.com>
2015-11-16 15:22:39 +1300

    Remove DefaultPlatformProvider and throw exception. Resolves #248

--- src/Caliburn.Micro/PlatformProvider.cs
+++ src/Caliburn.Micro/PlatformProvider.cs
@@ -1,15 +1,22 @@
-﻿namespace Caliburn.Micro {
+﻿using System;
+
+namespace Caliburn.Micro {
     /// <summary>
     /// Access the current <see cref="IPlatformProvider"/>.
     /// </summary>
     public static class PlatformProvider {
-        private static IPlatformProvider current = new DefaultPlatformProvider();
+        private static IPlatformProvider current;
 
         /// <summary>
         /// Gets or sets the current <see cref="IPlatformProvider"/>.
         /// </summary>
         public static IPlatformProvider Current {
-            get { return current; }
+            get
+            {
+                if (current == null)
+                    throw new InvalidOperationException("PlatformProvider.Current is not set, ensure Caliburn.Micro is correctly initialized");
+                return current;
+            }
             set { current = value; }
         }
     }
