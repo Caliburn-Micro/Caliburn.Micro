3630dccfebba2a314ef789adf619287c1369546e
Nigel Sampson <nigel.sampson@compiledexperience.com>
2016-03-04 09:27:10 +1300

    Increment AssemblyVersion. Resolves #299

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Resources;
 
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
