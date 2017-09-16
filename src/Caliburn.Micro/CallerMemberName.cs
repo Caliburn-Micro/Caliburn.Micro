e0dc7c846c2b02d45f841933517a74c408c65cd2
Nigel Sampson <nigel.sampson@compiledexperience.com>
2017-09-16 23:09:41 +1200

    #466 Add CallerMemberName to SL5 and NET40

---
+++ src/Caliburn.Micro/CallerMemberName.cs
@@ -0,0 +1,8 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Allows you to obtain the method or property name of the caller.
+    /// </summary>
+    [AttributeUsageAttribute(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class CallerMemberNameAttribute : Attribute { }
+}
