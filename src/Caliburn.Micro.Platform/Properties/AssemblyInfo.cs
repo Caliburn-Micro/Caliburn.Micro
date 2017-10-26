6242a8339e4c83f89e3f446b16ad311e39ead310
Nigel Sampson <nigel.sampson@pushpay.com>
2017-10-26 20:53:13 +1300

    #371 CLS Compliant false

--- src/Caliburn.Micro.Platform/Properties/AssemblyInfo.cs
+++ src/Caliburn.Micro.Platform/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows.Markup;
 [assembly: AssemblyTitle("Caliburn Micro Platform")]
 [assembly: AssemblyDescription("A small, yet powerful framework designed for Xaml platforms, Caliburn.Micro implements a variety of UI patterns for solving real-world problems. Patterns that are highlighted include MVVM (Presentation Model), MVP and MVC.")]
 
-[assembly: CLSCompliant(true)]
+[assembly: CLSCompliant(false)]
 
 #if NET
 [assembly: Guid("6449e9cb-4d4d-4d79-8f73-71a2fc647109")]
