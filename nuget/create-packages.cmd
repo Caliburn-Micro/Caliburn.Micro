7b69116c23a76b8ac700e44815f269b6b171a5a7
Nigel Sampson <nigel.sampson@pushpay.com>
2018-01-11 23:32:11 +1300

    #503 Remove Caliburn.Micro.Start

--- nuget/create-packages.cmd
+++ nuget/create-packages.cmd
@@ -3,4 +3,3 @@ del *.nupkg
 .\nuget.exe pack .\Caliburn.Micro.Core.nuspec -symbols
 .\nuget.exe pack .\Caliburn.Micro.nuspec -symbols
 .\nuget.exe pack .\Caliburn.Micro.Xamarin.Forms.nuspec -symbols
-.\nuget.exe pack .\Caliburn.Micro.Start.nuspec
