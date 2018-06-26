0ec9402ce67fcec73d39fc40d9890a5cdbf27a49
Nigel Sampson <nigel.sampson@pushpay.com>
2018-06-26 22:58:33 +1200

    #512 Async IGuardClose

--- src/Caliburn.Micro.Core/IGuardClose.cs
+++ src/Caliburn.Micro.Core/IGuardClose.cs
@@ -1,14 +1,17 @@
-﻿namespace Caliburn.Micro {
-    using System;
+﻿using System.Threading;
+using System.Threading.Tasks;
 
+namespace Caliburn.Micro
+{
     /// <summary>
     /// Denotes an instance which may prevent closing.
     /// </summary>
-    public interface IGuardClose : IClose {
+    public interface IGuardClose : IClose
+    {
         /// <summary>
         /// Called to check whether or not this instance can close.
         /// </summary>
         /// <param name="callback">The implementer calls this action with the result of the close check.</param>
-        void CanClose(Action<bool> callback);
+        Task<bool> CanCloseAsync(CancellationToken cancellationToken);
     }
 }
