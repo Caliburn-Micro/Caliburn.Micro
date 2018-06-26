1b38461eac874ee032bbfbc2473e5cd9fb7a4fa7
Nigel Sampson <nigel.sampson@pushpay.com>
2018-06-26 22:29:27 +1200

    #512 New close result
    We don't need a callback from the close strategy, but we now need a type for the result.

---
+++ src/Caliburn.Micro.Core/CloseResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Caliburn.Micro
+{
+    public class CloseResult<T> : ICloseResult<T>
+    {
+        public CloseResult(bool closeCanOccur, IEnumerable<T> children)
+        {
+            CloseCanOccur = closeCanOccur;
+            Children = children;
+        }
+        
+        public bool CloseCanOccur { get; }
+
+        public IEnumerable<T> Children { get; }
+    }
+}
---
+++ src/Caliburn.Micro.Core/ICloseResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Caliburn.Micro
+{
+    /// <summary>
+    /// Results from the close strategy.
+    /// </summary>
+    public interface ICloseResult<T>
+    {
+        /// <summary>
+        /// Indicates which children shbould close if the parent cannot.
+        /// </summary>
+        IEnumerable<T> Children { get; }
+
+        /// <summary>
+        /// Indicates whether a close can occur
+        /// </summary>
+        bool CloseCanOccur { get; }
+    }
+}
