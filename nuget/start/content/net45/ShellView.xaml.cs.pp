8847a980f9954298e65c1f67ce864e72e67e356f
qtuu <8949946+qtuu@users.noreply.github.com>
2018-10-26 00:17:26 +0200

    partial ShellView-class added
    inspired by the default VS itemtemplate for Windows

---
+++ nuget/start/content/net45/ShellView.xaml.cs.pp
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// Interaction logic for ShellView.xaml
+    /// </summary>
+    public partial class ShellView : Window
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
