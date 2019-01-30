8519bd6c4a3e20731f8e954afa860437a8d5000c
Nigel Sampson <nigel.sampson@pushpay.com>
2019-01-31 06:31:06 +1300

    #577 Fix build error

--- samples/features/Features.Forms/Features.Forms.Android/MainActivity.cs
+++ samples/features/Features.Forms/Features.Forms.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Features.CrossPlatform;
 
 namespace Features.Forms.Droid
 {
@@ -19,7 +20,7 @@ namespace Features.Forms.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            LoadApplication(new FormsApp());
         }
     }
 }
