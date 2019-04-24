c9ce93b4bc96a22a0e3d09dc024f15de7fc1172f
Nigel Sampson <nigel.sampson@pushpay.com>
2019-04-24 23:01:34 +1200

    #486 Add xmlns prefix for actual site

--- src/Caliburn.Micro.Platform/Platforms/net46/AssemblyInfo.cs
+++ src/Caliburn.Micro.Platform/Platforms/net46/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Windows.Markup;
 [assembly: Guid("6449e9cb-4d4d-4d79-8f73-71a2fc647109")]
 [assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.Micro")]
 [assembly: XmlnsPrefix("http://www.caliburnproject.org", "cal")]
+[assembly: XmlnsDefinition("http://caliburnmicro.com", "Caliburn.Micro")]
+[assembly: XmlnsPrefix("http://caliburnmicro.com", "cm")]
