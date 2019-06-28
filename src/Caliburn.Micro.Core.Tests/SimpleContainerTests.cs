df005d5a7d31b4039ae029cd73256a6361045a6d
Nigel Sampson <nigel.sampson@pushpay.com>
2019-06-28 14:42:14 +1200

    #372 Add unit tests for SimpleContainer

--- src/Caliburn.Micro.Core.Tests/SimpleContainerTests.cs
+++ src/Caliburn.Micro.Core.Tests/SimpleContainerTests.cs
@@ -123,4 +123,93 @@ namespace Caliburn.Micro.Core.Tests
             Assert.Equal(2, results.Count());
         }
     }
+
+    public class SimpleContainer_Find_Constructor
+    {
+
+        public class SingleEmptyConstructorType
+        {
+            public SingleEmptyConstructorType()
+            {
+
+            }
+        }
+
+        [Fact]
+        public void Container_Finds_Single_Constructor()
+        {
+            var container = new SimpleContainer();
+            container.Singleton<SingleEmptyConstructorType>();
+            container.GetInstance(typeof(SingleEmptyConstructorType), null);
+        }
+
+        public class SingleNonEmptyConstructorType
+        {
+            public SingleNonEmptyConstructorType(SimpleContainer_Find_Constructor.SingleEmptyConstructorType type)
+            {
+            }
+        }
+
+        [Fact]
+        public void Container_No_EmptyConstructor()
+        {
+            var container = new SimpleContainer();
+            container.Singleton<SingleNonEmptyConstructorType>();
+            container.GetInstance(typeof(SingleNonEmptyConstructorType), null);
+        }
+
+        public class SingleIntConstructor
+        {
+            public int Value { get; private set; }
+
+            public SingleIntConstructor(int x)
+            {
+                this.Value = x;
+            }
+        }
+
+        [Fact]
+        public void Container_SingleIntConstructor()
+        {
+            var container = new SimpleContainer();
+            container.Singleton<SingleIntConstructor>();
+            container.RegisterInstance(typeof(int), "x", 4);
+            var inst = (SingleIntConstructor)container.GetInstance(typeof(SingleIntConstructor), null);
+            Assert.Equal(4, inst.Value);
+        }
+
+        public class TwoConstructors
+        {
+            public int Value { get; set; }
+
+            public TwoConstructors()
+            {
+                this.Value = 42;
+            }
+
+            public TwoConstructors(int value)
+            {
+                Value = value;
+            }
+        }
+
+        [Fact]
+        public void Container_ChooseConstructorWithRegisteredParameter()
+        {
+            var container = new SimpleContainer();
+            container.Singleton<TwoConstructors>();
+            container.RegisterInstance(typeof(int), null, 23);
+            var inst = (TwoConstructors)container.GetInstance(typeof(TwoConstructors), null);
+            Assert.Equal(23, inst.Value);
+        }
+
+        [Fact]
+        public void Container_ChooseEmptyConstructorWithoutRegisteredParameter()
+        {
+            var container = new SimpleContainer();
+            container.Singleton<TwoConstructors>();
+            var inst = (TwoConstructors)container.GetInstance(typeof(TwoConstructors), null);
+            Assert.Equal(42, inst.Value);
+        }
+    }
 }
