bb3dfe638e9911144e9f19d386616d7263cd8016
KasperSK <raizzat@gmail.com>
2020-09-23 23:03:38 +0200

    Fixes the parsing of negative numbers in action parameters

---
+++ src/Caliburn.Micro.Platform.Tests/ParserTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Caliburn.Micro.Platform.Tests
+{
+    public class ParserTests
+    {
+        [Fact]
+        public void CreateParametersWithOddNumbers()
+        {
+            var evenResult = Parser.CreateParameter(null, "0.1");
+            var oddResult = Parser.CreateParameter(null, "-0.1");
+            var nanResult = Parser.CreateParameter(null, "-0.1abc");
+
+            Assert.Equal(0.1, double.Parse((string)evenResult.Value));
+            Assert.Equal(-0.1, double.Parse((string)oddResult.Value));
+            Assert.Null(nanResult.Value);
+        }
+    }
+}
