c169ca2dce0ddd52e8d9a53d2e593b328c0aba19
Ken Tucker <vb2ae@live.com>
2020-11-27 12:20:47 -0500

    Added .net 5 wpf sample

---
+++ samples/features/Features.NetFive/Views/ExecuteView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Features.CrossPlatform.Views
+{
+    public partial class ExecuteView
+    {
+        public ExecuteView()
+        {
+            InitializeComponent();
+        }
+
+        public void UpdateView()
+        {
+            Output.Text = "View Updated";
+        }
+    }
+}
