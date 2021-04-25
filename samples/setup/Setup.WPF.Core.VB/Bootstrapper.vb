92540b13ae4ee8ac0ed78cff247ddd143478e4c6
Ken Tucker <vb2ae@live.com>
2021-04-25 09:40:34 -0400

    VB Setup samples

---
+++ samples/setup/Setup.WPF.Core.VB/Bootstrapper.vb
@@ -0,0 +1,35 @@
+﻿Imports Caliburn.Micro
+
+Public Class Bootstrapper
+    Inherits BootstrapperBase
+
+    Private container As SimpleContainer
+
+    Public Sub New()
+        Initialize()
+    End Sub
+
+    Protected Overrides Sub Configure()
+        container = New SimpleContainer()
+
+        container.Singleton(Of IWindowManager, WindowManager)()
+
+        container.PerRequest(Of ShellViewModel)()
+    End Sub
+
+    Protected Overrides Sub OnStartup(sender As Object, e As StartupEventArgs)
+        DisplayRootViewFor(Of ShellViewModel)()
+    End Sub
+
+    Protected Overrides Function GetInstance(service As Type, key As String) As Object
+        Return container.GetInstance(service, key)
+    End Function
+
+    Protected Overrides Function GetAllInstances(service As Type) As IEnumerable(Of Object)
+        Return container.GetAllInstances(service)
+    End Function
+
+    Protected Overrides Sub BuildUp(instance As Object)
+        container.BuildUp(instance)
+    End Sub
+End Class
---
+++ samples/setup/Setup.WPF.VB/Views/ShellView.xaml.vb
@@ -0,0 +1,3 @@
+﻿Class ShellView
+
+End Class
