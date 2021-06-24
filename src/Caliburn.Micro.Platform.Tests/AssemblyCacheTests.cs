4fded7f4c905b2550f2a67972d4cfb0bd83dc266
Hadi Eskandari <h.eskandari@gmail.com>
2021-06-24 20:14:25 +1000

    add failing test for the assembly cache

---
+++ src/Caliburn.Micro.Platform.Tests/AssemblyCacheTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xunit;
+
+namespace Caliburn.Micro.Platform.Tests
+{
+    public class AssemblyCacheTests
+    {
+        [Fact]
+        public void AddingTheSameAssemblyMoreThanOnceShouldNotThrow()
+        {
+            AssemblySourceCache.Install();
+
+            var testAssembly = typeof(AssemblyCacheTests).Assembly;
+            AssemblySource.Instance.Add(testAssembly);
+
+            //Re-add the same assembly
+            var exception = Record.Exception(() => AssemblySource.Instance.Add(testAssembly));
+            Assert.Null(exception);
+        }
+    }
+
+    public class TestScreen : Screen
+    {
+    }
+}
