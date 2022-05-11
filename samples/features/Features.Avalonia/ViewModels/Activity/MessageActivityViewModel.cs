9d1ead560c0e1a65bb20b867af1e77641a202775
Ken Tucker <vb2ae@live.com>
2022-05-11 07:47:15 -0400

    features app for avalonia

---
+++ samples/features/Features.Avalonia/ViewModels/Activity/MessageActivityViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Features.Avalonia.ViewModels.Activity
+{
+    public class MessageActivityViewModel : ActivityBaseViewModel
+    {
+        public MessageActivityViewModel(string message)
+        {
+            Message = message;
+        }
+
+        public override string Title => "Message";
+        public string Message { get; }
+    }
+}
---
+++ samples/features/Features.Avalonia/ViewModels/ShellViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Features.Avalonia.ViewModels;
+
+namespace Features.Avalonia.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        private readonly SimpleContainer container;
+        private INavigationService navigationService;
+
+        public ShellViewModel(SimpleContainer container, INavigationService navigation)
+        {
+            this.container = container;
+            navigationService = navigation;
+        }
+
+        public void RegisterFrame(Frame frame)
+        {
+            navigationService.NavigateToViewModel(typeof(MenuViewModel));
+        }
+    }
+
+}
