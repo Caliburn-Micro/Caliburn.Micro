ffb4f17bbcf2c5c85f9864a1bde3d5c169592422
Dan Walmsley <dan@walms.co.uk>
2023-03-16 17:51:59 +0000

    remove reactiveui (shouldnt depend on another mvvm framework)

--- samples/setup/Setup.Avalonia/Views/MainView.axaml.cs
+++ samples/setup/Setup.Avalonia/Views/MainView.axaml.cs
@@ -2,25 +2,20 @@ using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using ReactiveUI;
 using Setup.Avalonia.ViewModels;
 
 namespace Setup.Avalonia.Views
 {
-    public partial class MainView : UserControl, IViewFor<MainViewModel>
+    public partial class MainView : UserControl
     {
         public MainView()
         {
             InitializeComponent();
         }
 
-        public MainViewModel? ViewModel { get; set; }
-        object? IViewFor.ViewModel { get; set; }
-
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
-
     }
 }
