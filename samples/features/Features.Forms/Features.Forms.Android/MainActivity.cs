11fce79f8095b657d8a37b999e252428c75e75a1
Ken Tucker <vb2ae@live.com>
2023-12-16 20:37:46 -0500

    fix build

--- samples/features/Features.Forms/Features.Forms.Android/MainActivity.cs
+++ samples/features/Features.Forms/Features.Forms.Android/MainActivity.cs
@@ -1,13 +1,13 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Features.CrossPlatform;
 using Caliburn.Micro;
+using Features.CrossPlatform;
 
 namespace Features.Forms.Droid
 {
     [Activity(Label = "Features.Forms", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
