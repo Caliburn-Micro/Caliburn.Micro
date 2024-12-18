8d62fc48c546888522085dcc9cd7f72e8110c105
Ken Tucker <vb2ae@live.com>
2024-12-18 16:49:04 -0500

    Refactor and reformat Maui and Xaml platform providers
    Refactored `BeginOnUIThread` in `MauiPlatformProvider.cs` to discard
    the result of `dispatcher.RunAsync` using the discard operator `_`.
    Reformatted `XamlPlatformProvider.cs` to place opening braces `{` on
    new lines for namespace, class, constructor, and several methods.
    Updated `BeginOnUIThread` to use the discard operator `_`. Improved
    indentation and formatting in various methods for better readability.

--- src/Caliburn.Micro.Platform/XamlPlatformProvider.cs
+++ src/Caliburn.Micro.Platform/XamlPlatformProvider.cs
@@ -1,4 +1,5 @@
-﻿namespace Caliburn.Micro {
+﻿namespace Caliburn.Micro
+{
     using System;
     using System.Collections.Generic;
     using System.Threading;
@@ -19,7 +20,8 @@
     /// <summary>
     /// A <see cref="IPlatformProvider"/> implementation for the XAML platfrom.
     /// </summary>
-    public class XamlPlatformProvider : IPlatformProvider {
+    public class XamlPlatformProvider : IPlatformProvider
+    {
 #if WINDOWS_UWP
         private readonly CoreDispatcher dispatcher;
 #else
@@ -29,7 +31,8 @@
         /// <summary>
         /// Initializes a new instance of the <see cref="XamlPlatformProvider"/> class.
         /// </summary>
-        public XamlPlatformProvider() {
+        public XamlPlatformProvider()
+        {
 #if WINDOWS_UWP
             dispatcher = Window.Current.Dispatcher;
 #elif AVALONIA
@@ -47,16 +50,19 @@
         /// <summary>
         /// Indicates whether or not the framework is in design-time mode.
         /// </summary>
-        public virtual bool InDesignMode {
+        public virtual bool InDesignMode
+        {
             get { return View.InDesignMode; }
         }
 
-        private void ValidateDispatcher() {
+        private void ValidateDispatcher()
+        {
             if (dispatcher == null)
                 throw new InvalidOperationException("Not initialized with dispatcher.");
         }
 
-        private bool CheckAccess() {
+        private bool CheckAccess()
+        {
 #if WINDOWS_UWP
             return dispatcher == null || Window.Current != null;
 #else
@@ -68,10 +74,11 @@
         /// Executes the action on the UI thread asynchronously.
         /// </summary>
         /// <param name="action">The action to execute.</param>
-        public virtual void BeginOnUIThread(System.Action action) {
+        public virtual void BeginOnUIThread(System.Action action)
+        {
             ValidateDispatcher();
 #if WINDOWS_UWP
-            var dummy = dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => action());
+            _ = dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => action());
 #elif AVALONIA
             dispatcher.Post(action);
 #else
@@ -84,7 +91,8 @@
         /// </summary>
         /// <param name="action">The action to execute.</param>
         /// <returns></returns>
-        public virtual Task OnUIThreadAsync(Func<Task> action) {
+        public virtual Task OnUIThreadAsync(Func<Task> action)
+        {
             ValidateDispatcher();
 #if WINDOWS_UWP
             return dispatcher.RunTaskAsync(action);
@@ -100,19 +108,24 @@
         /// </summary>
         /// <param name="action">The action to execute.</param>
         /// <exception cref="System.NotImplementedException"></exception>
-        public virtual void OnUIThread(System.Action action) {
+        public virtual void OnUIThread(System.Action action)
+        {
             if (CheckAccess())
                 action();
-            else {
+            else
+            {
 #if WINDOWS_UWP
                 dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => action()).AsTask().Wait();
 #else
                 Exception exception = null;
-                System.Action method = () => {
-                    try {
+                System.Action method = () =>
+                {
+                    try
+                    {
                         action();
                     }
-                    catch(Exception ex) {
+                    catch (Exception ex)
+                    {
                         exception = ex;
                     }
                 };
@@ -120,7 +133,7 @@
                 dispatcher.Invoke(method);
 
                 if (exception != null)
-                throw new System.Reflection.TargetInvocationException("An error occurred while dispatching a call to the UI Thread", exception);
+                    throw new System.Reflection.TargetInvocationException("An error occurred while dispatching a call to the UI Thread", exception);
 #endif
             }
         }
@@ -138,7 +151,8 @@
         /// The WindowManager marks that element as a framework-created element so that it can determine what it created vs. what was intended by the developer.
         /// Calling GetFirstNonGeneratedView allows the framework to discover what the original element was.
         /// </remarks>
-        public virtual object GetFirstNonGeneratedView(object view) {
+        public virtual object GetFirstNonGeneratedView(object view)
+        {
             return View.GetFirstNonGeneratedView(view);
         }
 
@@ -158,9 +172,11 @@
         /// </summary>
         /// <param name="view">The view.</param>
         /// <param name="handler">The handler.</param>
-        public virtual void ExecuteOnFirstLoad(object view, Action<object> handler) {
+        public virtual void ExecuteOnFirstLoad(object view, Action<object> handler)
+        {
             var element = view as FrameworkElement;
-            if (element != null && !(bool) element.GetValue(PreviouslyAttachedProperty)) {
+            if (element != null && !(bool)element.GetValue(PreviouslyAttachedProperty))
+            {
                 element.SetValue(PreviouslyAttachedProperty, true);
                 View.ExecuteOnLoad(element, (s, e) => handler(s));
             }
@@ -171,9 +187,11 @@
         /// </summary>
         /// <param name="view">The view.</param>
         /// <param name="handler">The handler.</param>
-        public virtual void ExecuteOnLayoutUpdated(object view, Action<object> handler) {
+        public virtual void ExecuteOnLayoutUpdated(object view, Action<object> handler)
+        {
             var element = view as FrameworkElement;
-            if (element != null) {
+            if (element != null)
+            {
                 View.ExecuteOnLayoutUpdated(element, (s, e) => handler(s));
             }
         }
@@ -190,7 +208,8 @@
         /// <exception cref="System.NotImplementedException"></exception>
         public virtual Func<CancellationToken, Task> GetViewCloseAction(object viewModel, ICollection<object> views, bool? dialogResult)
         {
-            foreach (var contextualView in views) {
+            foreach (var contextualView in views)
+            {
                 var viewType = contextualView.GetType();
 #if WINDOWS_UWP
                 var closeMethod = viewType.GetRuntimeMethod("Close", new Type[0]);
@@ -200,7 +219,8 @@
                 var closeMethod = viewType.GetMethod("Close", new Type[0]);
 #endif
                 if (closeMethod != null)
-                    return ct => {
+                    return ct =>
+                    {
 #if AVALONIA
                         if (dialogResult != null)
                         {
@@ -234,7 +254,8 @@
 #else
                 var isOpenProperty = viewType.GetProperty("IsOpen");
 #endif
-                if (isOpenProperty != null) {
+                if (isOpenProperty != null)
+                {
                     return ct =>
                     {
                         isOpenProperty.SetValue(contextualView, false, null);
