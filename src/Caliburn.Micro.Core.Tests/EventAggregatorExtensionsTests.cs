d7eb668bc6cdfaa229376c2c48c5ee29eaf84ea5
Kenneth Tucker <10343116@seaworld.com>
2025-08-18 07:51:34 -0400

    fix build and codeql suggestions

--- src/Caliburn.Micro.Core.Tests/EventAggregatorExtensionsTests.cs
+++ src/Caliburn.Micro.Core.Tests/EventAggregatorExtensionsTests.cs
@@ -7,10 +7,7 @@ namespace Caliburn.Micro.Core.Tests
 {
     public class EventAggregatorExtensionsTests
     {
-
-
-
-
+        const int NumberOfMockTargets = 20;
         [Fact]
         public void SubscribeOnUIThread_CallsSubscribeWithUIThreadMarshaller()
         {
@@ -24,9 +21,9 @@ namespace Caliburn.Micro.Core.Tests
             mockAggregator.Object.SubscribeOnUIThread(subscriber);
 
             Assert.NotNull(capturedMarshaller);
-            // UI thread marshaller returns a Task
-            var tcs = new TaskCompletionSource<bool>();
-            var task = capturedMarshaller(() => { tcs.SetResult(true); return tcs.Task; });
+            // UI thread marshaler returns a Task
+            var taskSource = new TaskCompletionSource<bool>();
+            var task = capturedMarshaller(() => { taskSource.SetResult(true); return taskSource.Task; });
             Assert.IsAssignableFrom<Task>(task);
         }
 
@@ -35,15 +32,18 @@ namespace Caliburn.Micro.Core.Tests
         {
             var eventAggregator = new EventAggregator();
             string message = "Test";
-            var cancellationToken = new CancellationTokenSource().Token;
-            var mockTarget1 = new Mock<IHandle<string>>();
-            var mockTarget2 = new Mock<IHandle<string>>();
-            eventAggregator.SubscribeOnPublishedThread(mockTarget1.Object);
-            eventAggregator.SubscribeOnPublishedThread(mockTarget2.Object);
-            await eventAggregator.PublishOnCurrentThreadAsync(message, cancellationToken);
+            using (var tokenSource = new CancellationTokenSource())
+            {
+                var cancellationToken = tokenSource.Token;
+                var mockTarget1 = new Mock<IHandle<string>>();
+                var mockTarget2 = new Mock<IHandle<string>>();
+                eventAggregator.SubscribeOnPublishedThread(mockTarget1.Object);
+                eventAggregator.SubscribeOnPublishedThread(mockTarget2.Object);
+                await eventAggregator.PublishOnCurrentThreadAsync(message, cancellationToken);
 
-            mockTarget1.Verify(x => x.HandleAsync(message, cancellationToken), Times.Once);
-            mockTarget2.Verify(x => x.HandleAsync(message, cancellationToken), Times.Once);
+                mockTarget1.Verify(x => x.HandleAsync(message, cancellationToken), Times.Once);
+                mockTarget2.Verify(x => x.HandleAsync(message, cancellationToken), Times.Once);
+            }
         }
 
 
@@ -52,14 +52,17 @@ namespace Caliburn.Micro.Core.Tests
         {
             var eventAggregator = new EventAggregator();
             var message = new object();
-            var cancellationToken = new CancellationTokenSource().Token;
-            var mockTarget1 = new Mock<IHandle<object>>();
-            eventAggregator.SubscribeOnPublishedThread(mockTarget1.Object);
+            using (var tokenSource = new CancellationTokenSource())
+            {
+                var cancellationToken = tokenSource.Token;
+                var mockTarget1 = new Mock<IHandle<object>>();
+                eventAggregator.SubscribeOnPublishedThread(mockTarget1.Object);
 
 
-            await eventAggregator.PublishOnBackgroundThreadAsync(message, cancellationToken);
+                await eventAggregator.PublishOnBackgroundThreadAsync(message, cancellationToken);
 
-            mockTarget1.Verify(x => x.HandleAsync(message, cancellationToken), Times.Once);
+                mockTarget1.Verify(x => x.HandleAsync(message, cancellationToken), Times.Once);
+            }
         }
 
         [Fact]
@@ -67,25 +70,25 @@ namespace Caliburn.Micro.Core.Tests
         {
             var eventAggregator = new EventAggregator();
             string message = "Test";
-            var cancellationToken = new CancellationTokenSource().Token;
-
-            // Create 20 mock targets
-            var mockTargets = new Mock<IHandle<object>>[20];
-            for (int i = 0; i < mockTargets.Length; i++)
+            using (var tokenSource = new CancellationTokenSource())
             {
-            var mockTargets = new Mock<IHandle<string>>[20];
-            for (int i = 0; i < mockTargets.Length; i++)
-            {
-                mockTargets[i] = new Mock<IHandle<string>>();
-                eventAggregator.SubscribeOnPublishedThread(mockTargets[i].Object);
-            }
+                var cancellationToken = tokenSource.Token;
 
-            await eventAggregator.PublishOnCurrentThreadAsync(message, cancellationToken);
+                // Create 20 mock targets
+                var mockTargets = new Mock<IHandle<string>>[NumberOfMockTargets];
+                for (int i = 0; i < mockTargets.Length; i++)
+                {
+                    mockTargets[i] = new Mock<IHandle<string>>();
+                    eventAggregator.SubscribeOnPublishedThread(mockTargets[i].Object);
+                }
 
-            // Verify HandleAsync was called on each
-            foreach (var mockTarget in mockTargets)
-            {
-                mockTarget.Verify(x => x.HandleAsync(message, cancellationToken), Times.Once);
+                await eventAggregator.PublishOnCurrentThreadAsync(message, cancellationToken);
+
+                // Verify HandleAsync was called on each
+                foreach (var mockTarget in mockTargets)
+                {
+                    mockTarget.Verify(x => x.HandleAsync(message, cancellationToken), Times.Once);
+                }
             }
         }
     }
