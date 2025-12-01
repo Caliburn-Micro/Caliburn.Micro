f956ae438ef9676ee38f822e4b4201e14a9a210d
Ken Tucker <vb2ae@live.com>
2025-12-01 05:55:18 -0500

    fix build

--- src/Caliburn.Micro.UWP/Properties/AssemblyInfo.cs
+++ src/Caliburn.Micro.UWP/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -21,6 +20,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
